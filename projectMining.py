5589cbb93f631f7191bbad12d6c89bfbe33445a6
IdaBagusAgung <101696743+IdaBagusAgung@users.noreply.github.com>
2024-06-26 04:01:58 +0800

    Update projectMining.py

--- projectMining.py
+++ projectMining.py
@@ -1,7 +1,6 @@
 import streamlit as st
 import pickle
-# !pip install scikit-learn
-# from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.feature_extraction.text import TfidfVectorizer
 
 # Load the pickle models
 model_nb = pickle.load(open('nb_model.pkl', 'rb'))
